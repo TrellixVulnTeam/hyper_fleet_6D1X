78442a524d37aa18777241e40ce2430560733351
Roman Figurin <r.v.figurin@gmail.com>
2017-11-25 10:55:00 +0200

    start scenario written to README

--- api/controllers/iotDataController.js
+++ api/controllers/iotDataController.js
@@ -6,11 +6,17 @@ var mongoose = require('mongoose'),
 
 exports.create_a_iotdata = function(req, res) {
   var new_iot_data = new IotData(req.body);
+
+  console.info('received object: ' + new_iot_data.name);
+  res.json('data received! ' + new_iot_data.name);
+
+  /*
   new_iot_data.save(function(err, data) {
     if (err)
       res.send(err);
     res.json(data);
   });
+  */
 };
 
 exports.list = function(req, res) {
